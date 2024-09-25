cbec0933dfa02e18a518b9e95b1c4f04e71e7bee
Tushar Daiya <tushardaiya@outlook.com>
2024-09-25 09:40:08 +0530

    server initial commit

---
+++ server/src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
