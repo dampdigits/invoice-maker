c20c8cf3da0a4c5b7d2f4dc057b225f42a5ef737
Tushar Daiya <tushardaiya@outlook.com>
2024-09-25 13:32:04 +0530

    add business and invoice db model

---
+++ invoice-maker/server/src/models/business.model.js
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from "mongoose";
+
+const BusinessSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  address: {
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zipCode: { type: String, required: true },
+    country: { type: String, required: true },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  taxId: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Business = mongoose.model("Business", BusinessSchema);
+
+export default Business;
---
+++ invoice-maker/server/src/models/invoice.model.js
@@ -0,0 +1,59 @@
+import mongoose, { Schema } from "mongoose";
+const InvoiceSchema = new mongoose.Schema({
+  business: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Business",
+    required: true,
+  },
+  customer: {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    address: {
+      street: { type: String, required: true },
+      city: { type: String, required: true },
+      state: { type: String, required: true },
+      zipCode: { type: String, required: true },
+    },
+  },
+  items: [
+    {
+      description: { type: String, required: true },
+      quantity: { type: Number, required: true },
+      price: { type: Number, required: true },
+      total: { type: Number, required: true },
+    },
+  ],
+  invoiceNumber: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  dateIssued: {
+    type: Date,
+    default: Date.now,
+  },
+  dueDate: {
+    type: Date,
+    required: true,
+  },
+  subtotal: {
+    type: Number,
+    required: true,
+  },
+  tax: {
+    type: Number,
+  },
+  totalAmount: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["Pending", "Paid", "Overdue"],
+    default: "Pending",
+  },
+});
+
+const Invoice = mongoose.model("Invoice", InvoiceSchema);
+
+export default Invoice;
