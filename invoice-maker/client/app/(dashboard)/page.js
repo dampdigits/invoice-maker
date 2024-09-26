2b31c605c4c318a6d4497a41ec5c545de8663165
Tushar Daiya <tushardaiya@outlook.com>
2024-09-26 13:51:08 +0530

    fixes cors

--- invoice-maker/client/app/(dashboard)/page.js
+++ invoice-maker/client/app/(dashboard)/page.js
@@ -5,7 +5,7 @@ import InvoicesTable from "@/components/InvoicesTable";
 import { Button } from "@/components/ui/button";
 async function fetchData() {
   const cookie = cookies().get("accessToken");
-  const res = await fetch(`http://localhost:4000/api/v1/analytics/`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/analytics/`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -17,21 +17,22 @@ async function fetchData() {
 }
 async function fetchInvoices() {
   const cookie = cookies().get("accessToken");
-  const res = await fetch(`http://localhost:4000/api/v1/invoice/getAll`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Cookie: cookie.name + "=" + cookie.value,
-    },
-  });
-  console.log(res);
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/invoice/getAll`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Cookie: cookie.name + "=" + cookie.value,
+      },
+    }
+  );
   const data = await res.json();
   return data.data;
 }
 export default async function page() {
   const data = await fetchData();
   const invoices = await fetchInvoices();
-  console.log(data);
   return (
     <div className="p-5">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 h-44 gap-5">
